4cf29769a1e43836f998b7c23b2e557a98fff23d
Border55-repo <juliannordli@gmail.com>
2026-09-22 23:58:47 +0200

    Bump KOVA Companion to v0.9.3 manifest hotfix

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -21,8 +21,8 @@ android {
         applicationId = "no.juliannordli.kovacomp"
         minSdk = 26
         targetSdk = 35
-        versionCode = 13
-        versionName = "0.9.2"
+        versionCode = 14
+        versionName = "0.9.3"
     }
 
     buildFeatures {
