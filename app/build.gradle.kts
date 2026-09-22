383b07aa73c93751fe2dc759fb5144610350f9c4
Border55-repo <juliannordli@gmail.com>
2026-09-22 15:45:43 +0200

    Bump Android app to v0.2.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -10,8 +10,8 @@ android {
         applicationId = "no.juliannordli.kovacomp"
         minSdk = 26
         targetSdk = 35
-        versionCode = 2
-        versionName = "0.1.1"
+        versionCode = 3
+        versionName = "0.2.0"
     }
     buildFeatures { compose = true }
 
