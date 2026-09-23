48379b2b04569975a00dd9564e779de7df71bfbd
Border55-repo <juliannordli@gmail.com>
2026-09-23 10:15:03 +0200

    Bump KOVA Companion to 0.13.4

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -21,8 +21,8 @@ android {
         applicationId = "no.juliannordli.kovacomp"
         minSdk = 26
         targetSdk = 36
-        versionCode = 24
-        versionName = "0.13.3"
+        versionCode = 25
+        versionName = "0.13.4"
     }
 
     buildFeatures {
