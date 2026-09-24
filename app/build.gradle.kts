df103a2a988f022f3d1289ad3f6a3bf6bb4c5c53
Border55-repo <juliannordli@gmail.com>
2026-09-24 15:23:22 +0200

    Bump Android to 2.0.2 for synchronized update

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -21,8 +21,8 @@ android {
         applicationId = "no.juliannordli.kovacomp"
         minSdk = 26
         targetSdk = 36
-        versionCode = 36
-        versionName = "2.0.1"
+        versionCode = 37
+        versionName = "2.0.2"
     }
 
     buildFeatures {
